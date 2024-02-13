29a3f7d6fe2e6cf6175c25878de2845fc9236d61
szdgr09 <szdgr09@gmail.com>
2024-02-13 22:05:32 +0800

    change image

--- src/components/ProfileImage/ProfileImage.jsx
+++ src/components/ProfileImage/ProfileImage.jsx
@@ -1,10 +1,14 @@
 import { ImageHolder, ImageMainWapper } from "./ProfileImage.styles";
-import profile from "../../assets/images/mainProfile.jpg";
+
 const ProfileImage = () => {
   return (
     <ImageMainWapper>
       <ImageHolder>
-        <img src={profile} />
+        <img
+          src={
+            "https://media.licdn.com/dms/image/D5603AQEr1mLV4QXWww/profile-displayphoto-shrink_800_800/0/1698287256828?e=1713398400&v=beta&t=pCBJwbJ623ldrQcrNVfJoRdM4GjsoD8YYUmiL8NyQhQ"
+          }
+        />
       </ImageHolder>
     </ImageMainWapper>
   );
