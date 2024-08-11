9a98b310d257daa472cd25a17e79fdeaae8f4166
szdgr09 <szdgr09@gmail.com>
2024-08-11 18:08:47 +0800

    navbar

--- src/layout/MainBody.jsx
+++ src/layout/MainBody.jsx
@@ -13,13 +13,13 @@ import React from "react";
 import { StyledBox } from "./MainBody.styles";
 import Design from "./Particles/Particles";
 // import NavBar from "./Navbar/NavBar";
-import Navbar from "@/layout/Navbar/Navbar";
 import { Grid } from "@mui/material";
 import About from "@/pages/About/About";
 import Technologies from "@/pages/Technologies/Technologies";
 import Experiences from "@/pages/Experiences/Experiences";
 import Contact from "@/pages/Contact/Contact";
 import Footer from "@/pages/Footer/Footer";
+import Navbar from "@/layout/Navbar/Navbar";
 
 const MainBody = () => {
   return (
