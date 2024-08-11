174c1dbd9550ae0696bcb8757a96d610bfa58aff
szdgr09 <szdgr09@gmail.com>
2024-08-11 16:28:05 +0800

    test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,34 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: {
+    react: { version: "18.2" },
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".vue", ".ts", ".d.ts"],
+      },
+      alias: {
+        extensions: [".vue", ".js", ".ts", ".scss", ".d.ts"],
+        map: [
+          ["@/components", "./src/components"],
+          ["@/pages", "./src/pages"],
+          ["@/router", "./src/router"],
+          ["@/store", "./src/store"],
+          ["@/styles", "./src/styles"],
+          ["@/types", "./src/types"],
+          ["@/utils", "./src/utils"],
+        ],
+      },
+    },
   },
-}
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": "warn",
+  },
+};
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 import * as path from "path";
-import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,12 +9,7 @@ export default defineConfig({
     port: 8080,
   },
   resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: fileURLToPath(new URL("./src", import.meta.url)),
-      },
-    ],
+    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
   plugins: [
     svgr(),
