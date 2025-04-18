747f0f71287c0507c132484a4d98e6e973b4bb98
szdgr09 <szdgr09@gmail.com>
2025-04-19 00:21:17 +0800

    hero header

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Box, Grid, Typography } from "@mui/material";
 import { StyledGrid } from "./About.styles";
+import { styled } from "@mui/material/styles";
 import ProfileImage from "../../components/ProfileImage/ProfileImage";
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
 import SectionSubtitle from "../../components/SectionSubtitle/SectionSubtitle";
@@ -8,6 +9,76 @@ import SectionSubtitle from "../../components/SectionSubtitle/SectionSubtitle";
 const shadow = {
   textShadow: "0 0 6px #DE935E,0 0 10px #DE935E,0 0 15px #DE935E",
 };
+
+const Button = ({ children }) => {
+  return (
+    <StyledWrapper>
+      <Typography
+        className="btnWhite"
+        href="#"
+        fontWeight={500}
+        component="p"
+        variant="h3"
+        color="secondary"
+      >
+        {children}
+      </Typography>
+    </StyledWrapper>
+  );
+};
+
+const StyledWrapper = styled("div")(({ theme }) => ({
+  ".btnWhite": {
+    display: "inline-flex",
+    alignItems: "center",
+    color: "#fff",
+    // border: "1px solid #fff",
+    minHeight: "50px",
+    // padding: "0 20px",
+    textTransform: "uppercase",
+    zIndex: 1,
+    position: "relative",
+    backgroundColor: "transparent",
+  },
+
+  // ".btnWhite::before": {
+  //   content: '""',
+  //   backgroundColor: "#fff",
+  //   display: "block",
+  //   width: "45px",
+  //   height: "1px",
+  //   marginRight: "20px",
+  // },
+
+  ".btnWhite::after": {
+    content: '""',
+    backgroundColor: "#fff",
+    display: "block",
+    width: "0%",
+    height: "100%",
+    right: 0,
+    top: 0,
+    position: "absolute",
+    zIndex: -1,
+    transition: "all 0.3s ease-out",
+  },
+
+  ".btnWhite:hover": {
+    color: "#232963",
+    textDecoration: "none",
+    transition: "all 0.3s ease-out",
+  },
+
+  ".btnWhite:hover::after": {
+    width: "100%",
+    transition: "all 0.3s ease-out",
+  },
+
+  ".btnWhite:hover::before": {
+    backgroundColor: "#232963",
+    transition: "all 0.3s ease-out",
+  },
+}));
 
 const Highlighted = ({ l, ...props }) => (
   <Box component="span" sx={shadow} {...props}>
@@ -26,13 +97,15 @@ const About = () => {
         spacing={2}
       >
         <Grid item xs={12}>
-          <SectionSubtitle label="Introduction" />
-          <SectionTitle label="Who Am I?" />
+          <SectionSubtitle label="Hervinson M. Samson" />
+          <SectionTitle label={<Button>Software Engineer</Button>} />
         </Grid>
+
         <Grid item xs={12} sm={12} md={6}>
+          {/* <Button>Hervinson Samson</Button> */}
           <Typography paddingY={2} variant="h6" color="text.primary">
-            -- <Highlighted l="Hizon" /> here (that's my nickname btw). I
-            specialize primarily in frontend development utilizing{" "}
+            <Highlighted l="Hizon" /> (that's my nickname btw). I specialize
+            primarily in frontend development utilizing{" "}
             <Highlighted l="ReactJS" /> as a software engineer. Throughout my
             career, I have gained extensive experience in software engineering
             across diverse sectors, including manufacturing, hospitality, and
